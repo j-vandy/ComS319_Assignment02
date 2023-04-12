7f1b7e73bc289d59dfb41047a541e6e79ce5e7d9
jvandy <jacksonvan2003@gmail.com>
2023-04-12 02:01:17 -0500

    Went crazy

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function displayCartView() {
   document.getElementById("BrowseView").style.display = 'none';
   document.getElementById("CartView").style.display = 'block';
   document.getElementById("ConfirmationView").style.display = 'none';
+  updateCart();
   return false;
 }
 
@@ -19,7 +20,12 @@ function displayConfirmationView() {
   return false;
 }
 
+// TODO need to reset the cart list when you go back to BrowseView
+// Update productAndPrice to also include description
+
 var cart = [];
+var productAndPrice = [["Cat1", 100], ["Cat2", 200], ["Cat3", 300], ["Cat4", 400], ["Cat5", 500], ["Cat6", 600]];
+var totalCost = 0;
 
 function addToCart(num)
 {
@@ -33,11 +39,65 @@ function removeFromCart(num)
     cart.splice(index, 1);
 }
 
+function updateCart()
+{
+  document.getElementById('cartCount').innerHTML = cart.length;
+
+  var numItems = [0,0,0,0,0,0];
+  for (let i = 0; i < cart.length; i++)
+  {
+    numItems[cart[i]]++;
+  }
+  for (let i = 0; i < numItems.length; i++)
+  {
+    createListElement(i, numItems[i]);
+  }
+  var ul = document.getElementById("list");
+  var li = document.createElement("li");
+  li.className = "list-group-item d-flex justify-content-between";
+  var span = document.createElement("span");
+  span.textContent = "Total (USD)";
+  li.appendChild(span);
+  var strong = document.createElement("strong");
+  strong.textContent = "$" + totalCost;
+  li.appendChild(strong);
+  ul.appendChild(li);
+}
+
+function clearCart()
+{
+
+}
+
+function createListElement(item, count)
+{
+  if (count <= 0 || item > 5 || item < 0) return false;
+  var ul = document.getElementById("list");
+  var li = document.createElement("li");
+  li.className = "list-group-item d-flex justify-content-between lh-sm";
+  var div = document.createElement("div");
+  var h6 = document.createElement("h6");
+  h6.className = "my-0";
+  h6.textContent = productAndPrice[item][0];
+  div.appendChild(h6);
+  var small = document.createElement("small");
+  small.className = "text-muted";
+  small.textContent = "Brief description";
+  div.appendChild(small);
+  li.appendChild(div);
+  var span = document.createElement("span");
+  span.className = "text-muted";
+  span.textContent = "$" + productAndPrice[item][1] + " x " + count;
+  totalCost += productAndPrice[item][1] * count;
+  li.appendChild(span);
+  ul.appendChild(li);
+}
+
 var count1 = count2 = count3 = count4 = count5 = count6 = 0;
 
 function incrementCounter(num)
 {
-  addToCart(num);
+  addToCart(num - 1);
   if(num == 1)
   {
     const counter1 = document.getElementById('counter1');
@@ -83,7 +143,7 @@ function decrementCounter(num)
   {
     if (count1 > 0)
     {
-      removeFromCart(num);
+      removeFromCart(num - 1);
       const counter1 = document.getElementById('counter1');
       count1--;
       counter1.innerHTML = count1;
@@ -93,7 +153,7 @@ function decrementCounter(num)
   {
     if (count2 > 0)
     {
-      removeFromCart(num);
+      removeFromCart(num - 1);
       const counter2 = document.getElementById('counter2');
       count2--;
       counter2.innerHTML = count2;
@@ -103,7 +163,7 @@ function decrementCounter(num)
   {
     if (count3 > 0)
     {
-      removeFromCart(num);
+      removeFromCart(num - 1);
       const counter3 = document.getElementById('counter3');
       count3--;
       counter3.innerHTML = count3;
@@ -113,7 +173,7 @@ function decrementCounter(num)
   {
     if (count4 > 0)
     {
-      removeFromCart(num);
+      removeFromCart(num - 1);
       const counter4 = document.getElementById('counter4');
       count4--;
       counter4.innerHTML = count4;
@@ -123,7 +183,7 @@ function decrementCounter(num)
   {
     if (count5 > 0)
     {
-      removeFromCart(num);
+      removeFromCart(num - 1);
       const counter5 = document.getElementById('counter5');
       count5--;
       counter5.innerHTML = count5;
@@ -133,7 +193,7 @@ function decrementCounter(num)
   {
     if (count6 > 0)
     {
-      removeFromCart(num);
+      removeFromCart(num - 1);
       const counter6 = document.getElementById('counter6');
       count6--;
       counter6.innerHTML = count6;
